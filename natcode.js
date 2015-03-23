a38cd25e2e43bdf6c7f0f30ea16bfac8d6d5c50b
Abner Yang <abtyang@gmail.com>
2015-03-23 16:57:39 -0400

    removed natcode.js

--- natcode.js
+++
@@ -1,5 +0,0 @@
-this.run = function(){
-	var dist=Math.floor(Math.random()*this.end)
-console.log(this.name + " " this.food + " sprinted " + dist + " meters! Go " this.name + " " + this.food + "!")
-	return dist
-}

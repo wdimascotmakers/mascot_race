6f7e557d532e62169432abf00085f218cde28398
huckpilot <huckpilot@gmail.com>
2015-03-23 15:55:05 -0400

    I updated the mascot function

--- mascot_race.js
+++ mascot_race.js
@@ -11,6 +11,18 @@ var penny = new Mascot("Penny","Prune","3");
 var reggie = new Mascot("Reggie","rice cake","2");
 var carl = new Mascot("Carl","Clamato","4");
 
-raceDistance = function(distance, mascot1, mascot2, mascot3 ) {
-	
+var race = function(race_distance, mascot1, mascot2, mascot3 ) {
+	var distanceOne = race_distance;
+	var distanceTwo = race_distance;
+	var distanceThree = race_distance;
+	var distOneLeft = distanceOne - penny.run();
+	var distTwoLeft = distanceTwo - reggie.run();
+	var disThreeLeft =  distanceThree - carl.run();
+	console.log('distOneLeft');
+	console.log('distTwoLeft');
+	console.log('distThreeLeft');
+	if() {
+		
+	}
+
 }
